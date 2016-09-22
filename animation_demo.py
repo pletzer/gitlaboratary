b6925170774f76c868b60c308c73f82beaff5908
williamsjh <jonny.williams@niwa.co.nz>
2016-09-22 05:00:12 +0000

    Adding comment to enable running at NIWA

--- animation_demo.py
+++ animation_demo.py
@@ -4,6 +4,49 @@ Pyplot animation example.
 The method shown here is only for very simple, low-performance
 use.  For more demanding applications, look at the animation
 module and the examples that use it.
+
+Note that currently (22/9/16) to run this at NIWA out-of-the-box, you will need to run with Ananconda or similar. This can be achieved by typing the following:
+
+module load python/anaconda-2.4.1-python-2.7
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+x = np.arange(0, 2*np.pi, 0.01)
+line, = ax.plot(x, np.sin(x))
+
+
+def animate(i):
+    line.set_ydata(np.sin(x + i/10.0))  # update the data
+    return line,
+
+
+# Init only required for blitting to give a clean slate.
+def init():
+    line.set_ydata(np.ma.array(x, mask=True))
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
+                              interval=25, blit=True)
+plt.show()"""
+A simple example of an animated plot
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+fig, ax = plt.subplots()
+
+x = np.arange(0, 2*np.pi, 0.01)
+line, = ax.plot(x, np.sin(x))
+
+
+def animate(i):
+
 """
 
 import matplotlib.pyplot as plt
